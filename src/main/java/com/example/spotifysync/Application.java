998aa1740c2cfa2bfdbf74e228577a29af8ca146
Kush Rastogi <krastogi@nianticlabs.com>
2020-04-05 16:08:18 -0700

    adding cookie for state

--- src/main/java/com/example/spotifysync/Application.java
+++ src/main/java/com/example/spotifysync/Application.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.UUID;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
@@ -26,33 +30,55 @@ public class Application {
 
   @RequestMapping(value = "/authenticate", method = RequestMethod.GET)
   public void authenticateWithSpotify(HttpServletResponse httpServletResponse) {
-    //		var state = generateRandomString(16);
-    //		set state cookie (later)
+		final String state = UUID.randomUUID().toString();
+		Cookie stateCookie = new Cookie("state", state);
+		stateCookie.setPath("/");
+		stateCookie.setSecure(true);
+		stateCookie.setHttpOnly(true);
 
-    // your application requests authorization
+		// your application requests authorization
     final String responseType = "code";
     final String scope = "user-read-currently-playing";
     final String clientId = getSpotifyClientId();
     final String redirectUri = getServerUrl() + "callback";
     final String spotifyAuthLink =
         String.format(
-            "https://accounts.spotify.com/authorize?response_type=%s&client_id=%s&scope=%s&redirect_uri=%s&state=state",
+            "https://accounts.spotify.com/authorize?response_type=%s&client_id=%s&scope=%s&redirect_uri=%s&state=%s",
             responseType,
             clientId,
             scope,
-            redirectUri
+            redirectUri,
+						state
         );
 
+    httpServletResponse.addCookie(stateCookie);
     httpServletResponse.setHeader("Location", spotifyAuthLink);
     httpServletResponse.setStatus(302);
   }
 
 	@RequestMapping(value = "/callback", method = RequestMethod.GET)
 	public String callback(
+			@CookieValue(value = "state", defaultValue = "") String storedState,
 			@RequestParam(name = "code", required = false, defaultValue = "") String code,
 			@RequestParam(name = "state", required = true, defaultValue = "") String state,
 			@RequestParam(name = "error", required = false, defaultValue = "") String error,
-			Model model) {
+			Model model,
+			HttpServletResponse httpServletResponse
+	) {
+
+  	//Removing state cookie
+		Cookie stateCookie = new Cookie("state", null);
+		stateCookie.setMaxAge(0);
+		stateCookie.setSecure(true);
+		stateCookie.setHttpOnly(true);
+		stateCookie.setPath("/");
+		//add cookie to response
+		httpServletResponse.addCookie(stateCookie);
+
+  	if (!storedState.equals(state)) {
+  		System.out.println("Stored state does not match state returned from Spotify");
+			System.out.println("Authorization failed. States do not match.");
+		}
   	if (!code.equals(""))  {
   		System.out.println("Authorization successful. Auth Token: " + code);
 			model.addAttribute("name", "token: " + code);
