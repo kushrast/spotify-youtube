7d6ed124aa4f92e947f0c3e0da5780efb7803daf
Kush Rastogi <krastogi@nianticlabs.com>
2020-04-05 15:05:24 -0700

    Adding Application

--- src/main/java/com/example/spotifysync/Application.java
+++ src/main/java/com/example/spotifysync/Application.java
@@ -2,9 +2,17 @@ package com.example.spotifysync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class Application {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
