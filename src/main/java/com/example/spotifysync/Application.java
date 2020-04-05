0979b311819be833db90879c27ad18875fcc269a
Kush Rastogi <krastogi@nianticlabs.com>
2020-04-05 15:39:21 -0700

    Adding endpoint for spotify authorization

--- src/main/java/com/example/spotifysync/Application.java
+++ src/main/java/com/example/spotifysync/Application.java
@@ -5,20 +5,63 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @SpringBootApplication
 public class Application {
+  final String SPOTIFY_CLIENT_ID_VAR_KEY = "SPOTIFY_CLIENT_ID";
 
-	@GetMapping("/")
-	public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
+  @GetMapping("/")
+  public String home(
+      @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+      Model model) {
+    model.addAttribute("name", name);
+    return "index";
+  }
+
+  @RequestMapping(value = "/authenticate", method = RequestMethod.GET)
+  public void authenticateWithSpotify(HttpServletResponse httpServletResponse) {
+    //		var state = generateRandomString(16);
+    //		set state cookie (later)
+
+    // your application requests authorization
+    final String responseType = "code";
+    final String scope = "user-read-currently-playing";
+    final String clientId = getSpotifyClientId();
+    final String redirectUri = getServerUrl() + "callback";
+    final String spotifyAuthLink =
+        String.format(
+            "https://accounts.spotify.com/authorize?response_type=%s&client_id=%s&scope=%s&redirect_uri=%s&state=state",
+            responseType,
+            clientId,
+            scope,
+            redirectUri
+        );
+
+    httpServletResponse.setHeader("Location", spotifyAuthLink);
+    httpServletResponse.setStatus(302);
+  }
+
+	@RequestMapping(value = "/callback", method = RequestMethod.GET)
+	public String callback(Model model) {
+  	model.addAttribute("authenticated");
 		return "index";
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  private String getSpotifyClientId() {
+    return System.getenv(SPOTIFY_CLIENT_ID_VAR_KEY);
+  }
 
+  private String getServerUrl() {
+    return "https://spotify-youtube.herokuapp.com/";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
