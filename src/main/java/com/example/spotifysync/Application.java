3256d47751703738354a5c3e9dc6364b76a0c190
Kush Rastogi <krastogi@nianticlabs.com>
2020-04-10 21:08:06 -0700

    resolving paths for css and js

--- src/main/java/com/example/spotifysync/Application.java
+++ src/main/java/com/example/spotifysync/Application.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,86 @@
+var syncRequest = null;
+var playbackData = null;
+var player = null;
+
+function onLoad() {
+  $( "#sync" ).hide();
+  var refresh_token = Cookies.get("refresh_token");
+  if (refresh_token != null && refresh_token != "") {
+    $( "#not_authenticated" ).remove();
+    $( "#sync" ).show();
+  }
+}
+
+function sync() {
+  var spotifyUri = playbackData != null ? playbackData["spotifyUri"] : "";
+  var oldYouTubeId = playbackData != null ? playbackData["youTubeId"] : "";
+  console.log(oldYouTubeId);
+  $.getJSON({
+    url: "/update",
+    data: {
+      "spotifyUri": spotifyUri
+    }
+  })
+    .done(function( data ) {
+      console.log( "data:", data);
+      playbackData = data;
+
+      $("#youtubePlayer").remove();
+
+      var progress = data["progressSeconds"];
+      var youTubeId = data["youTubeId"] != null ? data["youTubeId"] : oldYouTubeId;
+      var sameVideo = youTubeId == oldYouTubeId;
+
+      playbackData["youTubeId"] = youTubeId;
+
+      if (youTubeId == "") {
+        alert("encountered error while getting latest state from server.");
+      } else {
+        updateYouTubePlayer(sameVideo, youTubeId, progress, data["currentlyPlaying"]);
+    }
+  });
+}
+
+function updateYouTubePlayer(isSameVideo, videoId, progress, currentlyPlaying) {
+  if (isSameVideo) {
+    player.seekTo(progress, true);
+  } else {
+    player.loadVideoById(videoId, progress);
+  }
+
+  console.log("currentlyPlaying: " + currentlyPlaying);
+  if (currentlyPlaying) {
+        console.log("Play");
+    player.playVideo();
+  } else {
+        console.log("Pause");
+    player.pauseVideo();
+  }
+  player.mute();
+}
+
+function onYouTubePlayerAPIReady() {
+  var refresh_token = Cookies.get("refresh_token");
+  if (refresh_token != null && refresh_token != "") {
+    player = new YT.Player('youTubePlayer', {
+      height: '720',
+      width: '1280',
+      videoId: 'xWggTb45brM',
+    });
+  }
+}
+
+function toggleSync(e) {
+  if (syncRequest != null) {
+    $("#sync").html("Start syncing");
+    clearInterval(syncRequest);
+    syncRequest = null;
+  } else {
+    $("#sync").html("Stop sync");
+    sync();
+    syncRequest = setInterval(sync, 15000);
+  }
+}
+$("#sync").on("click", toggleSync);
+
+onLoad();
