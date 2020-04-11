33c12db04d4f486040d4dcdc5905cff7722ee21e
Kush Rastogi <krastogi@nianticlabs.com>
2020-04-10 23:26:50 -0700

    Adding js loading spinner

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,6 +2,27 @@ var syncRequest = null;
 var playbackData = null;
 var player = null;
 
+var opts = {
+  lines: 5, // The number of lines to draw
+  length: 38, // The length of each line
+  width: 17, // The line thickness
+  radius: 45, // The radius of the inner circle
+  scale: 1, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  color: '#ffffff', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  speed: 1, // Rounds per second
+  rotate: 0, // The rotation offset
+  animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  className: 'spinner', // The CSS class to assign to the spinner
+  top: '50%', // Top position relative to parent
+  left: '50%', // Left position relative to parent
+  shadow: '0 0 1px transparent', // Box-shadow for the lines
+  position: 'absolute' // Element positioning
+};
+
 function onLoad() {
     $("body").css("background","rgb(255,221,225)");
     $("body").css("background","linear-gradient(90deg, rgba(255,221,225,1) 0%, rgba(238,156,167,1) 100%)");
@@ -12,12 +33,11 @@ function onLoad() {
     $("body").css("background-color","black");
     $( "#not_authenticated" ).remove();
     $( "#authenticated" ).show();
-      $("#loading_circle").hide();
   }
 }
 
 function sync() {
-  $("#loading_circle").show();
+  var spinner = new Spinner(opts).spin($("#loading_circle").get());
   var spotifyUri = playbackData != null ? playbackData["spotifyUri"] : "";
   var oldYouTubeId = playbackData != null ? playbackData["youTubeId"] : "";
   console.log(oldYouTubeId);
@@ -45,7 +65,7 @@ function sync() {
       updateYouTubePlayer(sameVideo, youTubeId, progress, data["currentlyPlaying"]);
   }
 });
-  setTimeout(function(){$("#loading_circle").hide()}, 1500);
+  setTimeout(function(){spinner.stop()}, 1500);
 }
 
 function updateYouTubePlayer(isSameVideo, videoId, progress, currentlyPlaying) {
