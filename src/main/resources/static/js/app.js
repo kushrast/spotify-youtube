b3ae4d3c88880f7f726c47dbde356bd0f293e561
Kush Rastogi <krastogi@nianticlabs.com>
2020-04-10 21:53:31 -0700

    forgot paren

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -6,7 +6,7 @@ function onLoad() {
   $("#authenticated").hide();
   var refresh_token = Cookies.get("refresh_token");
   if (refresh_token != null && refresh_token != "") {
-    $(body).css("background-color","black");
+    $('body').css("background-color","black");
     $( "#not_authenticated" ).remove();
     $( "#authenticated" ).show();
   }
